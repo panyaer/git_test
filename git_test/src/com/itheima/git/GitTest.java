827fe9e57886285288e2013a414d3db93aa78463
hehe <hehe@itheima.com>
2019-02-15 19:22:00 +0800

    本地工程git_test添加到git

---
+++ git_test/src/com/itheima/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello idea");
+        System.out.println("hello git");
+        System.out.println("boom~~~boom");
+    }
+}
